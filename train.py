2856914366ebf5217bfc98d0bb7987b507e3da0c
Jeremy Fell <jeremyafell@gmail.com>
2022-06-23 11:01:17 +0200

    Refactor the training script and create new training loop classes
    Cleans up the main train.py script.
    Moves all training loops to a new training.py script.
    Refactors the training loops into classes that are subclasses of a new EpisodicTraining class to reduce code duplication.
    Adds DQNTraining, PPOTraining, and WOLPTraining classes which are refactored training loops.

--- train.py
+++ train.py
@@ -1,71 +1,88 @@
-import traci
-import timeit
-import numpy as np
-import random
-from State import TL_list
-from utils import *
+import heapq
 import os
 import sys
-import datetime
-import heapq
-import pandas as pd
-from pandas import DataFrame
-from utils import import_configuration
 from shutil import copyfile
-from collections import deque
-import torch
+
 import matplotlib.pyplot as plt
-from pathlib import Path
+import numpy as np
+from pandas import DataFrame
+
 from environment import SUMO
+from training import DQNTraining, PPOTraining, WOLPTraining
+from utils import import_configuration, create_train_path, create_test_path, add_master_data
+
 
 config = import_configuration()
 print(config)
+is_train = config['is_train']
 
 # Set up the corresponding SUMO environment for training or testing
-if config['is_train']:
+if is_train:
     env = SUMO()
 else:
-    from Statistics_vehicles import Statistic_Vehicles
-    vehicle_stats = Statistic_Vehicles()
+    from vehicle_statistics import VehicleStatistics
+    vehicle_stats = VehicleStatistics()
     env = SUMO(vehicle_stats)
 
 print('State shape: ', env.num_states)
 print('Number of actions: ', env.action_space.n)
 
-if config['agent_type'] == 'DQN':
-    from agents.dqn_agent import DQNAgent
-    agent = DQNAgent(env.num_states, env.action_space.n, config['hidden_dim'], config['fixed_action_space'], env.TL_list, config['memory_size_max'],
-                config['batch_size'], config['gamma'], config['tau'], config['learning_rate'], config['target_update'],
-                )
+# Create agent based on config file, and train it
+agent_type = config['agent_type']
+match agent_type:
+    case 'DQN':
+        from agents.dqn_agent import DQNAgent
+        agent = DQNAgent(
+            env.num_states, env.action_space.n, config['hidden_dim'], config['fixed_action_space'],
+            env.TL_list, config['memory_size_max'], config['batch_size'], config['gamma'], config['tau'],
+            config['learning_rate'], config['target_update'],
+        )
+        scores, training_times = DQNTraining(agent, env, 'dqn').train(is_train=is_train)
 
-if config['agent_type'] == 'PPO':
-    from agents.ppo_agent import PPOAgent
-    agent = PPOAgent(env.num_states, env.action_space.n, config['actor_dim'], config['critic_dim'], config['fixed_action_space'],
-                env.TL_list, config['batch_size'], config['n_epochs'], config['policy_clip'], config['gamma'], config['gae_lambda'],
-                config['policy_learning_rate'], config['value_learning_rate']
-                )
+    case 'PPO':
+        from agents.ppo_agent import PPOAgent
+        agent = PPOAgent(
+            env.num_states, env.action_space.n, config['actor_dim'], config['critic_dim'], config['fixed_action_space'],
+            env.TL_list, config['batch_size'], config['n_epochs'], config['policy_clip'], config['gamma'],
+            config['gae_lambda'], config['policy_learning_rate'], config['value_learning_rate']
+        )
+        scores, training_times = PPOTraining(agent, env, 'ppo').train(is_train=is_train)
 
-if config['agent_type'] == 'WOLP':
-    from deprecated.wolp_agent import WolpertingerAgent
-    agent = WolpertingerAgent(env.num_states, env.action_space.n, config['actor_dim'], config['critic_dim'], config['eps_policy'], config['actor_init_w'],
-                config['critic_init_w'], config['memory_size_max'], config['batch_size'], config['gamma'], config['tau'], config['ou_theta'], config['ou_mu'],
-                config['ou_sigma'], config['policy_learning_rate'], config['value_learning_rate'], config['weight_decay']
-                )
+    case 'WOLP':
+        from deprecated.wolp_agent import WolpertingerAgent
+        agent = WolpertingerAgent(
+            env.num_states, env.action_space.n, config['actor_dim'], config['critic_dim'], config['eps_policy'],
+            config['actor_init_w'], config['critic_init_w'], config['memory_size_max'], config['batch_size'],
+            config['gamma'], config['tau'], config['ou_theta'], config['ou_mu'], config['ou_sigma'],
+            config['policy_learning_rate'], config['value_learning_rate'], config['weight_decay']
+        )
+        scores, training_times = WOLPTraining(agent, env, 'wolp').train(is_train=is_train)
 
-if config['agent_type'] == 'MADQN':
-    from agents.madqn_agent import MADQNAgent
-    agent = MADQNAgent(env.num_states, env.action_space.n, len(env.TL_list), config['hidden_dim'], config['single_state_space'], config['local_reward_signal'],
-                env.TL_list, config['memory_size_max'], config['batch_size'], config['gamma'], config['tau'], config['learning_rate'], config['target_update']
-                )
+    case 'MADQN':
+        from agents.madqn_agent import MADQNAgent
+        agent = MADQNAgent(
+            env.num_states, env.action_space.n, len(env.TL_list), config['hidden_dim'], config['single_state_space'],
+            config['local_reward_signal'],
+            env.TL_list, config['memory_size_max'], config['batch_size'], config['gamma'], config['tau'],
+            config['learning_rate'], config['target_update']
+        )
+        scores, training_times = DQNTraining(agent, env, 'madqn').train(is_train=is_train)
 
-if config['agent_type'] == 'MAPPO':
-    from agents.mappo_agent import MAPPOAgent
-    agent = MAPPOAgent(env.num_states, env.action_space.n, len(env.TL_list), config['actor_dim'], config['critic_dim'], config['training_strategy'], config['actor_parameter_sharing'],
-                config['critic_parameter_sharing'], config['single_state_space'], config['local_reward_signal'], env.TL_list, config['batch_size'], config['n_epochs'],
-                config['policy_clip'], config['gamma'], config['gae_lambda'], config['policy_learning_rate'], config['value_learning_rate']
-                )
+    case 'MAPPO':
+        from agents.mappo_agent import MAPPOAgent
+        agent = MAPPOAgent(
+            env.num_states, env.action_space.n, len(env.TL_list), config['actor_dim'], config['critic_dim'],
+            config['training_strategy'], config['actor_parameter_sharing'], config['critic_parameter_sharing'],
+            config['single_state_space'], config['local_reward_signal'],env.TL_list, config['batch_size'],
+            config['n_epochs'], config['policy_clip'], config['gamma'], config['gae_lambda'],
+            config['policy_learning_rate'], config['value_learning_rate']
+        )
+        scores, training_times = PPOTraining(agent, env, 'mappo').train(is_train=is_train)
 
-if config['is_train']:
+    case _:
+        sys.exit(f'Invalid agent_type: {agent_type} is not an implemented agent')
+
+if is_train:
     path = create_train_path(config['models_path_name'])
     print('Training results will be saved in:', path)
     copyfile(src='training_settings.ini', dst=os.path.join(path, 'training_settings.ini'))
@@ -73,228 +90,34 @@ else:
     test_path, plot_path = create_test_path(config['test_model_path_name'])
     print('Test results will be saved in:', plot_path)
 
-# Define the SADQN or MADQN RL training loop
-def DQNRL(n_episodes=config['total_episodes'], max_t=config['max_steps'] + 1000, eps_start=config['eps_start'],
-          eps_end=config['eps_end'], eps_decay=config['eps_decay'], single_agent=config['single_agent']):
-    """Deep Q-Learning (Single-Agent and Multi-Agent).
-
-    Params
-    ======
-        n_episodes (int): maximum number of training episodes
-        max_t (int): maximum number of timesteps per episode
-        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
-        eps_end (float): minimum value of epsilon
-        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
-        single_agent (bool): whether to use single-agent or multi-agent implementation
-    """
-
-    timestamp_start = datetime.datetime.now()
-    training_time = []
-    scores = []  # list containing scores from each episode
-    scores_window = deque(maxlen=100)  # last 100 scores
-    eps = eps_start  # initialize epsilon
-    episode_start = 1 if config['is_train'] else n_episodes + 1
-    episode_end = n_episodes + 1 if config['is_train'] else n_episodes + 11
-    if not config['is_train']:
-        agent.load_models(os.path.join(test_path, 'dqn' if single_agent else 'madqn'))
-    for i_episode in range(episode_start, episode_end):
-        env._TrafficGen.generate_routefile(model_path=env.model_path, model_id=env.model_id, seed=i_episode)
-        if not config['is_train']:
-            vehicle_stats.create_stats(i_episode)
-        state = env.reset()
-        score = 0
-        for t in range(max_t):
-            if config['is_train']:
-                q_values, action = agent.act(np.array(state), eps)
-                next_state, reward, done, _ = env.step(action)
-                agent.step(state, action, reward, next_state, done)
-            else:
-                q_values, action = agent.act(np.array(state))
-                next_state, reward, done, _ = env.step(action)
-            state = next_state
-            score += sum(list(reward.values()))
-            if done:
-                break
-        scores_window.append(score)  # save most recent score
-        scores.append(score)  # save most recent score
-        training_time.append((datetime.datetime.now() - timestamp_start).total_seconds())
-        eps = max(eps_end, eps_decay * eps)  # decrease epsilon
-        print('\r                                       Episode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(
-            scores_window)), end="")
-        if i_episode % 100 == 0:
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
-
-        if not config['is_train']:
-            vehicle_stats.store_current_stats(plot_path)
-    if config['is_train']:
-        if not os.path.exists(os.path.join(path, 'dqn' if single_agent else 'madqn')):
-            os.makedirs(os.path.join(path, 'dqn' if single_agent else 'madqn'))
-        agent.save_models(os.path.join(path, 'dqn' if single_agent else 'madqn'))
-    env.reset()
-    env.close()
-    if not config['is_train']:
-        vehicle_stats.create_overview(plot_path)
-    return scores, training_time
-
-
-# Define the SAPPO or MAPPO RL training loop
-def PPORL(n_episodes=config['total_episodes'], max_t=config['max_steps'] + 1000, single_agent=config['single_agent']):
-    """Proximal Policy Optimization (Single-Agent and Multi-Agent).
-
-    Params
-    ======
-        n_episodes (int): maximum number of training episodes
-        max_t (int): maximum number of timesteps per episode
-        single_agent (bool): whether to use single-agent or multi-agent implementation
-    """
-
-    best_score = -100000
-    learn_every = 64
-    timestamp_start = datetime.datetime.now()
-    training_time = []
-    scores = []  # list containing scores from each episode
-    scores_window = deque(maxlen=100)  # last 100 scores
-    episode_start = 1 if config['is_train'] else n_episodes + 1
-    episode_end = n_episodes + 1 if config['is_train'] else n_episodes + 11
-    if not config['is_train']:
-        agent.load_models(os.path.join(test_path, 'ppo' if single_agent else 'mappo'))
-    for i_episode in range(episode_start, episode_end):
-        env._TrafficGen.generate_routefile(model_path=env.model_path, model_id=env.model_id, seed=i_episode)
-        if not config['is_train']:
-            vehicle_stats.create_stats(i_episode)
-        state = env.reset()
-        score = 0
-        for t in range(max_t):
-            action, prob, val = agent.choose_action(state)
-            state_, reward, done, info = env.step(action)
-            if config['is_train']:
-                agent.remember(state, action, prob, val, reward, done)
-                if t % learn_every == 0:
-                    agent.learn()
-            state = state_
-            score += sum(list(reward.values()))
-            if done:
-                break
-        scores_window.append(score)  # save most recent score
-        scores.append(score)  # save most recent score
-        training_time.append((datetime.datetime.now() - timestamp_start).total_seconds())
-        if score > best_score:
-            best_score = score
-        print('\r                                       Episode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(
-            scores_window)), end="")
-        if i_episode % 100 == 0:
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
-
-        if not config['is_train']:
-            vehicle_stats.store_current_stats(plot_path)
-    if config['is_train']:
-        if not os.path.exists(os.path.join(path, 'ppo' if single_agent else 'mappo')):
-            os.makedirs(os.path.join(path, 'ppo' if single_agent else 'mappo'))
-        agent.save_models(os.path.join(path, 'ppo' if single_agent else 'mappo'))
-    env.reset()
-    env.close()
-    if not config['is_train']:
-        vehicle_stats.create_overview(plot_path)
-    return scores, training_time
-
-
-# Define the WOLP RL training loop
-def WOLPRL(n_episodes=config['total_episodes'], max_t=config['max_steps'] + 1000, warmup=config['warmup']):
-    """Wolpertinger with Deep Deterministic Policy Gradient.
-
-    Params
-    ======
-        n_episodes (int): maximum number of training episodes
-        max_t (int): maximum number of timesteps per episode
-        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
-        eps_end (float): minimum value of epsilon
-        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
-        warmup (int): time without training but only filling the replay memory and for random action selection
-    """
-
-    update_every = 64
-    timestamp_start = datetime.datetime.now()
-    training_time = []
-    scores = []  # list containing scores from each episode
-    scores_window = deque(maxlen=100)  # last 100 scores
-    episode_start = 1 if config['is_train'] else n_episodes + 1
-    episode_end = n_episodes + 1 if config['is_train'] else n_episodes + 11
-    if not config['is_train']:
-        agent.load_models(os.path.join(test_path, 'wolp'))
-    for i_episode in range(episode_start, episode_end):
-        env._TrafficGen.generate_routefile(model_path=env.model_path, model_id=env.model_id, seed=i_episode)
-        if not config['is_train']:
-            vehicle_stats.create_stats(i_episode)
-        state = env.reset()
-        agent.reset()
-        score = 0
-        for t in range(max_t):
-            if agent.t_step > warmup * 10 or not config['is_train']:
-                raw_action, action = agent.select_action(np.array(state))
-            else:
-                raw_action, action = agent.random_action()
-            action = action.reshape(1,).astype(int)[0]
-            next_state, reward, done, _ = env.step(action)
-            if config['is_train']:
-                agent.observe(state, raw_action, reward, next_state, done)
-                agent.step()
-                if agent.t_step >= warmup and agent.t_step % update_every == 0:
-                    # for _ in range(update_every):
-                    agent.update_policy()
-            state = next_state
-            score += sum(list(reward.values()))
-            if done:
-                break
-        scores_window.append(score)  # save most recent score
-        scores.append(score)  # save most recent score
-        training_time.append((datetime.datetime.now() - timestamp_start).total_seconds())
-        print('\r                                       Episode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(
-            scores_window)), end="")
-        if i_episode % 100 == 0:
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
-
-        if not config['is_train']:
-            vehicle_stats.store_current_stats(plot_path)
-    if config['is_train']:
-        if not os.path.exists(os.path.join(path, 'wolp')):
-            os.makedirs(os.path.join(path, 'wolp'))
-        agent.save_models(os.path.join(path, 'wolp'))
-    env.reset()
-    env.close()
-    if not config['is_train']:
-        vehicle_stats.create_overview(plot_path)
-    return scores, training_time
-
-
-# Run the training
-if 'DQN' in config['agent_type']:
-    scores, training_time = DQNRL()
-
-if 'PPO' in config['agent_type']:
-    scores, training_time = PPORL()
-
-if config['agent_type'] == 'WOLP':
-    scores, training_time = WOLPRL()
-
+# Print model information
 print(f'Config: {config}')
 print(f'State shape: {env.num_states}')
 print(f'Number of actions: {env.action_space.n}')
 print(f'Model ID: {env.model_id}')
 print(f'Scores: {scores}')
-if config['total_episodes'] >= 50 and config['is_train']:
-    print(f'Smallest scores (n=50): {heapq.nlargest(50, scores)}')
-# print(env.waiting_time)
+BEST_SCORE_WINDOW = 50
+if config['total_episodes'] >= BEST_SCORE_WINDOW and is_train:
+    print(f'Smallest scores (n={BEST_SCORE_WINDOW}): {heapq.nlargest(BEST_SCORE_WINDOW, scores)}')
 
-# plot the scores
+data_path = path if is_train else plot_path
+
+# Plot the scores from training or testing
 fig = plt.figure()
 ax = fig.add_subplot(111)
 plt.plot(np.arange(len(scores)), scores)
 plt.ylabel('Score')
 plt.xlabel('Episode #')
-plt.savefig(os.path.join(path if config['is_train'] else plot_path, 'training_reward.png'))
+plt.savefig(os.path.join(data_path, 'training_reward.png'))
 # plt.show()
-DataFrame(data={"reward":scores}).to_csv(os.path.join(path if config['is_train'] else plot_path, 'reward.csv'), sep=',')
-DataFrame(data={"reward":scores, "training_time":training_time,
-               "waiting_time":env.waiting_time["TL"],"queue_length":env.queue["TL"]}
-         ).to_csv(os.path.join(path if config['is_train'] else plot_path, 'training_stats.csv'), sep=',', index=False)
-add_master_data(path if config['is_train'] else plot_path, config, scores, training_time, env.waiting_time["TL"], env.waiting_time["TL"])
+
+# Save the results from training or testing
+DataFrame(data={"reward": scores}).to_csv(os.path.join(data_path, 'reward.csv'), sep=',')
+DataFrame(data={
+    "reward": scores,
+    "training_time": training_times,
+    "waiting_time": env.waiting_time["TL"],
+    "queue_length": env.queue["TL"]
+}).to_csv(os.path.join(data_path, 'training_stats.csv'), sep=',', index=False)
+
+add_master_data(data_path, config, scores, training_times, env.waiting_time["TL"], env.waiting_time["TL"])
---
+++ training.py
@@ -0,0 +1,196 @@
+import datetime
+import os
+from shutil import copyfile
+from collections import deque
+
+import numpy as np
+
+from environment import SUMO
+from utils import import_configuration, create_train_path, create_test_path
+
+config = import_configuration()
+
+
+class EpisodicTraining:
+    TEST_EPISODES = 10
+    EPISODE_INTERVAL = 100
+
+    def __init__(self, agent, env: SUMO, model_name: str, n_episodes: int, max_t: int):
+        self.agent = agent
+        self.env = env
+        self.n_episodes = n_episodes
+        self.max_t = max_t
+        self.model_name = model_name
+
+    def run_episode(self, state, is_train: bool) -> int:
+        raise NotImplementedError('Must implement training "step" method')
+
+    def train(self, is_train: bool = True) -> tuple[list[int], list[float]]:
+        timestamp_start = datetime.datetime.now()
+        training_times = []
+        scores = []  # Contains all episode scores
+        scores_window = deque(maxlen=self.EPISODE_INTERVAL)  # Recent window of episode scores
+
+        if is_train:
+            path = create_train_path(config['models_path_name'])
+            model_path = os.path.join(path, self.model_name)
+            print('Training results will be saved in:', path)
+            copyfile(src='training_settings.ini', dst=os.path.join(path, 'training_settings.ini'))
+            episode_start = 1
+            episode_end = self.n_episodes + 1
+        else:
+            test_path, plot_path = create_test_path(config['test_model_path_name'])
+            test_model_path = os.path.join(test_path, self.model_name)
+            print('Test results will be saved in:', plot_path)
+            episode_start = self.n_episodes + 1
+            episode_end = self.n_episodes + self.TEST_EPISODES + 1
+            self.agent.load_models(test_model_path)
+
+        for i_episode in range(episode_start, episode_end):
+            self.env.generate_traffic(seed=i_episode)
+
+            if not is_train:
+                self.env.vehicle_stats.create(i_episode)
+
+            state = self.env.reset()
+            score = self.run_episode(state, is_train)
+
+            # Save most recent score
+            scores_window.append(score)
+            scores.append(score)
+
+            training_times.append((datetime.datetime.now() - timestamp_start).total_seconds())
+            print(
+                '\r                                       Episode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(
+                    scores_window)), end="")
+
+            if i_episode % self.EPISODE_INTERVAL == 0:
+                print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+
+            if not is_train:
+                self.env.vehicle_stats.save(plot_path)
+
+        if is_train and not os.path.exists(model_path):
+            os.makedirs(model_path)
+            self.agent.save_models(model_path)
+
+        self.env.reset()
+        self.env.close()
+
+        if not is_train:
+            self.env.vehicle_stats.generate_summary(plot_path)
+
+        return scores, training_times
+
+
+class DQNTraining(EpisodicTraining):
+    def __init__(
+            self,
+            agent,
+            env: SUMO,
+            model_name: str,
+            n_episodes: int = config['total_episodes'],
+            max_t: int = config['max_steps'] + 1000,
+            eps_start: float = config['eps_start'],
+            eps_end: float = config['eps_end'],
+            eps_decay: float = config['eps_decay'],
+    ):
+        super().__init__(agent, env, model_name, n_episodes, max_t)
+        self.eps = eps_start
+        self.eps_end = eps_end
+        self.eps_decay = eps_decay
+
+    def run_episode(self, state, is_train: bool = True) -> int:
+        score = 0
+
+        for t in range(self.max_t):
+            if is_train:
+                q_values, action = self.agent.act(np.array(state), self.eps)
+                next_state, reward, done, _ = self.env.step(action)
+                self.agent.step(state, action, reward, next_state, done)
+            else:
+                q_values, action = self.agent.act(np.array(state))
+                next_state, reward, done, _ = self.env.step(action)
+
+            state = next_state
+            score += sum(list(reward.values()))
+
+            if done:
+                break
+
+        self.eps = max(self.eps_end, self.eps_decay * self.eps)  # Decrease epsilon
+
+        return score
+
+
+class PPOTraining(EpisodicTraining):
+    LEARN_EVERY = 64
+
+    def __init__(
+            self,
+            agent,
+            env: SUMO,
+            model_name: str,
+            n_episodes: int = config['total_episodes'],
+            max_t: int = config['max_steps'] + 1000,
+    ):
+        super().__init__(agent, env, model_name, n_episodes, max_t)
+
+    def run_episode(self, state, is_train: bool = True) -> int:
+        score = 0
+
+        for t in range(self.max_t):
+            action, prob, val = self.agent.choose_action(state)
+            next_state, reward, done, info = self.env.step(action)
+
+            if is_train:
+                self.agent.remember(state, action, prob, val, reward, done)
+                if t % self.LEARN_EVERY == 0:
+                    self.agent.learn()
+            state = next_state
+            score += sum(list(reward.values()))
+            if done:
+                break
+
+        return score
+
+
+class WOLPTraining(EpisodicTraining):
+    UPDATE_EVERY = 64
+
+    def __init__(
+            self,
+            agent,
+            env: SUMO,
+            model_name: str,
+            n_episodes: int = config['total_episodes'],
+            max_t: int = config['max_steps'] + 1000,
+            warmup: int = config['warmup']
+    ):
+        super().__init__(agent, env, model_name, n_episodes, max_t)
+        self.warmup = warmup
+
+    def run_episode(self, state, is_train: bool = True) -> int:
+        score = 0
+        for t in range(self.max_t):
+            if self.agent.t_step > self.warmup * 10 or not is_train:
+                raw_action, action = self.agent.select_action(np.array(state))
+            else:
+                raw_action, action = self.agent.random_action()
+
+            action = action.reshape(1, ).astype(int)[0]
+            next_state, reward, done, _ = self.env.step(action)
+
+            if is_train:
+                self.agent.observe(state, raw_action, reward, next_state, done)
+                self.agent.step()
+
+                if self.agent.t_step >= self.warmup and self.agent.t_step % self.UPDATE_EVERY == 0:
+                    self.agent.update_policy()
+
+            state = next_state
+            score += sum(list(reward.values()))
+            if done:
+                break
+
+        return score
